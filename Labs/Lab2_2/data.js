cc8d97a28cc5ddf3afe9aadbf06a6ba89617b2ed
Timothy Barnard <timothy.barnard@mydit.ie>
2017-02-20 11:53:27 +0000

    Lab2

--- Labs/Lab2_2/data.js
+++ Labs/Lab2_2/data.js
@@ -1,56 +1,61 @@
-//const database = require('./database')();
+const database = require('./database')();
 
-module.exports.populate =function(){
+module.exports.populate =function(request, response){
 
-    var courtRoom1 = yield database.CourtRoom.build({
+    database.CourtRoom.sync();
+    database.Judge.sync();
+    database.Participant.sync();
+    database.Case.sync();
+
+    var courtRoom1 = database.CourtRoom.build({
         number: '101'
     }).save();
 
-    var courtRoom2 = yield database.CourtRoom.build({
+    var courtRoom2 = database.CourtRoom.build({
         number: '102'
     }).save();
 
-    var courtRoom2 = yield database.CourtRoom.build({
+    var courtRoom2 = database.CourtRoom.build({
         number: '102'
     }).save();
 
-    var judge1 = yield database.Judge.build({
+    var judge1 = database.Judge.build({
         name: "John",
         room: 999,
         ext: "111"
     }).save();
 
-    var judge2 = yield database.Judge.build({
+    var judge2 = database.Judge.build({
         name: "Mary",
         room: 888,
         ext: "112"
     }).save();
 
-    var judge3 = yield database.Judge.build({
+    var judge3 = database.Judge.build({
         name: "James",
         room: 777,
         ext: "113"
     }).save();
 
-    var participant1 = yield database.Participant.build({
+    var participant1 = database.Participant.build({
         name: "Smith",
         address: "1 Line, Dublin",
          type: "claimant"
     }).save();
 
-    var participant2 = yield database.Participant.build({
+    var participant2 = database.Participant.build({
         name: "Tim",
         address: "2 Dumbow, Dublin",
          type: "respondent"
     }).save();
 
-    var participant3 = yield database.Participant.build({
+    var participant3 = database.Participant.build({
         name: "Andy",
         address: "55 Test, Dublin",
         type: "claimant"
     }).save();
 
-    var case1 = yield database.Case.build({
+    var case1 = database.Case.build({
         judge_id: 0,
         courtroom_id: 0,
         claimant_id: 0,
@@ -59,7 +64,7 @@ module.exports.populate =function(){
         duration: 1,
     }).save();
 
-    var case2 = yield database.Case.build({
+    var case2 = database.Case.build({
         judge_id: 1,
         courtroom_id: 1,
         claimant_id: 0,
@@ -67,4 +72,6 @@ module.exports.populate =function(){
         start_date: Date("2017-05-02"),
         duration: 1
     }).save();
+
+    response.send("Successfull");
 }
